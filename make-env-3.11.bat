7178dca9d63437843306c4bb01115a838afec654
prashanth-acsq <prashanth@acsq.com.qa>
2024-02-19 12:08:51 +0300

    Fixed Errors

--- make-env-3.11.bat
+++ make-env-3.11.bat
@@ -1,1 +1,1 @@
-start /MAX cmd /c "cls && title Prepare Environment && py -3.11 -m venv venv311 && cd venv311/Scripts && activate && cd .. && cd .. && py -3.8 -m pip install --upgrade pip && pip install -r requirements.txt && deactivate && timeout /t 5 /nobreak"
+start /MAX cmd /c "cls && title Prepare Environment && py -3.11 -m venv venv311 && cd venv311/Scripts && activate && cd ../.. && python -m pip install --upgrade pip && pip install -r requirements.txt && deactivate && timeout /t 5 /nobreak"
