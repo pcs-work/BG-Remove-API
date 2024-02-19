50d1ab7a289465a6cf8eba4f20ae46fb13e8ee43
prashanth-acsq <prashanth@acsq.com.qa>
2024-02-19 11:10:25 +0300

    Updated tests + Formatting

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import app
 from sanic_testing.testing import SanicTestClient
 
@@ -7,43 +9,24 @@ test_client = SanicTestClient(app)
 def test_get_root():
     _, response = test_client.get("/")
     assert response.json == {
-            "statusText": "Root Endpoint of BG-Remove-API",
-            "version" : "1.0.0"
-        }
+        "statusText": "Root Endpoint of BG-Remove-API",
+    }
     assert response.status_code == 200
 
 
-def test_get_remove():
-    _, response = test_client.get("/remove")
+@pytest.mark.parametrize("mode", ["remove", "replace"])
+def test_get_processing(mode: str):
+    _, response = test_client.get(f"/{mode}")
     assert response.json == {
-            "statusText": "Background Removal Endpoint of BG-Remove-API",
-            "version" : "1.0.0"
-        }
+        "statusText": f"{mode.title()} endpoint of BG-Remove-API",
+    }
     assert response.status_code == 200
 
 
-def test_get_replace():
-    _, response = test_client.get("/replace")
+@pytest.mark.parametrize("mode", ["remove", "replace"])
+def test_get_processing_li(mode: str):
+    _, response = test_client.get(f"/{mode}/li")
     assert response.json == {
-            "statusText": "Background Replacement Endpoint of BG-Remove-API",
-            "version" : "1.0.0"
-        }
-    assert response.status_code == 200
-
-
-def test_get_remove_li():
-    _, response = test_client.get("/remove/li")
-    assert response.json == {
-            "statusText": "Lightweight Background Removal Endpoint of BG-Remove-API",
-            "version" : "1.0.0"
-        }
-    assert response.status_code == 200
-
-
-def test_get_replace_li():
-    _, response = test_client.get("/replace/li")
-    assert response.json == {
-            "statusText": "Lightweight Background Replacement Endpoint of BG-Remove-API",
-            "version" : "1.0.0"
-        }
+        "statusText": f"{mode.title()} lightweight endpoint of BG-Remove-API",
+    }
     assert response.status_code == 200
